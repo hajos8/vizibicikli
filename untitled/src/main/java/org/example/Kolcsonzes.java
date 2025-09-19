42bbbe35ef99ecfddad3164e0d63ddd3e31435e1
hajos8 <rabb.andras@gmail.com>
2025-09-19 08:39:37 +0200

    fixing

--- untitled/src/main/java/org/example/Kolcsonzes.java
+++ untitled/src/main/java/org/example/Kolcsonzes.java
@@ -1,4 +1,72 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Kolcsonzes {
+    private ArrayList<String[]> kolcsonzesek = new ArrayList<>();
+
+    public ArrayList<String[]> getKolcsonzesek() {
+        return kolcsonzesek;
+    }
+    public void setKolcsonzesek(ArrayList<String[]> kolcsonzesek) {
+        this.kolcsonzesek = kolcsonzesek;
+    }
+
+    public Kolcsonzes() {
+        setKolcsonzesek(this.kolcsonzesek);
+    }
+
+    public void fajleBeolvasa(String path) {
+        try{
+            Scanner fajlScanner = new Scanner(new File(path));
+
+            fajlScanner.nextLine(); //header miatt
+
+            while (fajlScanner.hasNextLine()) {
+                String sor = fajlScanner.nextLine();
+                String[] adatok = sor.split(";");
+
+                ArrayList<String[]> kolcsonzesek = getKolcsonzesek();
+                kolcsonzesek.add(adatok);
+                setKolcsonzesek(kolcsonzesek);
+
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Hiba a fájl beolvasásakor: " + e.getMessage());
+        }
+
+    }
+
+    public ArrayList<String[]> valakiKolcsonzesei(String nev){
+        ArrayList<String[]> valakiKolcsonzesei = new ArrayList<>();
+        for(String[] kolcsonzes : kolcsonzesek){
+            if(kolcsonzes[0].equalsIgnoreCase(nev)){
+                valakiKolcsonzesei.add(kolcsonzes);
+            }
+        }
+
+        return valakiKolcsonzesei;
+    }
+
+    public ArrayList<String[]> adottIdopont(String idopont){
+        ArrayList<String[]> vizenLevok = new ArrayList<>();
+        String[] idopontReszek = idopont.split(":");
+        for(String[] kolcsonzes : kolcsonzesek){
+            //Név;JAzon;EÓra;EPerc;VÓra;Vperc
+            if((Integer.parseInt(kolcsonzes[2]) < Integer.parseInt(idopontReszek[0])  //Eóra kisebb mint a megadott óra
+                    && Integer.parseInt(kolcsonzes[3]) < Integer.parseInt(idopontReszek[1])) //Eperc kisebb mint a megadott perc
+            &&
+            (Integer.parseInt(kolcsonzes[4]) > Integer.parseInt(idopontReszek[0] ) //Vóra nagyobb mint a megadott óra
+                    && Integer.parseInt(kolcsonzes[5]) > Integer.parseInt(idopontReszek[1]) //Vperc nagyobb mint a megadott perc
+            ))
+                vizenLevok.add(kolcsonzes);{
+            }
+        }
+
+        return vizenLevok;
+    }
 }
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,53 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static boolean isRunningTest = false;
+    public static String testNev;
+    public static String testIdopont;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Kolcsonzes kolcsonzes = new Kolcsonzes();
+
+        //4. feladat
+        kolcsonzes.fajleBeolvasa("kolcsonzesek.txt");
+
+        //5. feladat
+        System.out.println("5. feladat: Napi kölcsönzések száma: " + kolcsonzes.getKolcsonzesek().size() + " db");
+
+        //6. feladat
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("6. feladat: Kérek egy nevet: ");
+        String nev = isRunningTest ? testNev : sc.nextLine();
+
+        ArrayList<String[]> nevKolcsonzesei = kolcsonzes.valakiKolcsonzesei(nev);
+
+        System.out.println(nevKolcsonzesei.isEmpty() ? nev.substring(0, 1).toUpperCase() + nev.substring(1) + "kölcsönzései: \n Nem volt neki kölcsönzései még." : nev.substring(0, 1).toUpperCase() + nev.substring(1) + " kölcsönzései:");
+
+        for(String[] kolcsonzesei : nevKolcsonzesei){
+            System.out.println(kolcsonzesei[2] + ":" + (kolcsonzesei[3].length() == 1 ? "0" + kolcsonzesei[3] : kolcsonzesei[3])
+                    + "-" + kolcsonzesei[4] + ":"
+                    + (kolcsonzesei[5].length() == 1 ? "0" + kolcsonzesei[5] : kolcsonzesei[5]));
+        }
+
+        //7. feladat
+
+        System.out.print("7. feladat: Adjon meg egy időpontot óra:perc alakban: ");
+
+        String idopont = isRunningTest ? testIdopont : sc.nextLine();
+
+        ArrayList<String[]> vizenLevok = kolcsonzes.adottIdopont(idopont);
+
+        System.out.println(vizenLevok.isEmpty() ? "A vízen lévő járművek:\nNem volt vízen jármű épp." : "A vízen lévő járművek:");
+
+        for(String[] kolcsonzesei : vizenLevok){
+            System.out.println(kolcsonzesei[2] + ":" + (kolcsonzesei[3].length() == 1 ? "0" + kolcsonzesei[3] : kolcsonzesei[3])
+                    + "-" + kolcsonzesei[4] + ":"
+                    + (kolcsonzesei[5].length() == 1 ? "0" + kolcsonzesei[5] : kolcsonzesei[5]) +
+                    " : " + kolcsonzesei[0]);
+        }
     }
 }
