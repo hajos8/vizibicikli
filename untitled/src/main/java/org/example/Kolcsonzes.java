0d68622f40cd38bd5189a70bee7ed86ebd081d7c
hajos8 <rabb.andras@gmail.com>
2025-09-20 15:53:42 +0200

    Rewrited most of it, but its done

--- untitled/src/main/java/org/example/Kolcsonzes.java
+++ untitled/src/main/java/org/example/Kolcsonzes.java
@@ -8,118 +8,74 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Kolcsonzes {
-    private ArrayList<String[]> kolcsonzesek = new ArrayList<>();
+    private String nev;
+    private String jazon;
+    private int eOra;
+    private int ePerc;
+    private int vOra;
+    private int vPerc;
 
-    public ArrayList<String[]> getKolcsonzesek() {
-        return kolcsonzesek;
+    public String getNev() {
+        return nev;
     }
-    public void setKolcsonzesek(ArrayList<String[]> kolcsonzesek) {
-        this.kolcsonzesek = kolcsonzesek;
-    }
-
-    public Kolcsonzes() {
-        setKolcsonzesek(this.kolcsonzesek);
-    }
-
-    public void fajleBeolvasa(String path) {
-        try{
-            Scanner fajlScanner = new Scanner(new File(path));
-
-            fajlScanner.nextLine(); //header miatt
-
-            while (fajlScanner.hasNextLine()) {
-                String sor = fajlScanner.nextLine();
-                String[] adatok = sor.split(";");
-
-                ArrayList<String[]> kolcsonzesek = getKolcsonzesek();
-                kolcsonzesek.add(adatok);
-                setKolcsonzesek(kolcsonzesek);
-
-            }
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("Hiba a fájl beolvasásakor: " + e.getMessage());
-        }
-
+    public void setNev(String nev) {
+        this.nev = nev;
     }
 
-    public ArrayList<String[]> valakiKolcsonzesei(String nev){
-        ArrayList<String[]> valakiKolcsonzesei = new ArrayList<>();
-        for(String[] kolcsonzes : kolcsonzesek){
-            if(kolcsonzes[0].equalsIgnoreCase(nev)){
-                valakiKolcsonzesei.add(kolcsonzes);
-            }
-        }
-
-        return valakiKolcsonzesei;
+    public String getJazon() {
+        return jazon;
+    }
+    public void setJazon(String jazon) {
+        this.jazon = jazon;
     }
 
-    public ArrayList<String[]> adottIdopont(String idopont){
-        ArrayList<String[]> vizenLevok = new ArrayList<>();
-        String[] idopontReszek = idopont.split(":");
-        int keresettIdopont = Integer.parseInt(idopontReszek[0]) * 60 + Integer.parseInt(idopontReszek[1]);
-
-        for(String[] kolcsonzes : kolcsonzesek){
-            //Név;JAzon;EÓra;EPerc;VÓra;Vperc
-
-            int elvitte = Integer.parseInt(kolcsonzes[2]) * 60 + Integer.parseInt(kolcsonzes[3]);
-            int visszahozta = Integer.parseInt(kolcsonzes[4]) * 60 + Integer.parseInt(kolcsonzes[5]);
-
-            if(keresettIdopont >= elvitte && keresettIdopont < visszahozta){
-                vizenLevok.add(kolcsonzes);
-            }
-
-        }
-
-        return vizenLevok;
+    public int getEOra() {
+        return eOra;
+    }
+    public void setEOra(int eOra) {
+        this.eOra = eOra;
     }
 
-    public int napiBevetel(){
-        int bevetel = 0;
-        for(String[] kolcsonzes : kolcsonzesek){
-            int idoTartam =
-                    (Integer.parseInt(kolcsonzes[4]) * 60 + Integer.parseInt(kolcsonzes[5])) - //visszahozott idő percekben
-                            (Integer.parseInt(kolcsonzes[2]) * 60 + Integer.parseInt(kolcsonzes[3])); //elvitte idő percekben
-
-            bevetel += idoTartam / 30 + (idoTartam % 30 == 0 ? 0 : 1); //minden megkezdett fél óra 2400 ft
-        }
-
-        return bevetel * 2400;
+    public int getEPerc() {
+        return ePerc;
+    }
+    public void setEPerc(int ePerc) {
+        this.ePerc = ePerc;
     }
 
-    public void fajlbaIr(String path){
-        try{
-            FileWriter writer = new FileWriter(path);
-
-            for(String[] kolcsonzes : kolcsonzesek){
-                if (kolcsonzes[1].equals("F")) {
-                    writer.write(kolcsonzes[2] + ":" + kolcsonzes[3] + "-" +
-                            kolcsonzes[4] + ":" + kolcsonzes[5] + " : " +
-                            kolcsonzes[0] + "\n");
-                }
-
-
-            }
-
-            writer.close();
-        }
-        catch (Exception e){
-            System.out.println("Hiba a fájl írásakor: " + e.getMessage());
-        }
+    public int getVOra() {
+        return vOra;
+    }
+    public void setVOra(int vOra) {
+        this.vOra = vOra;
     }
 
-    public HashMap<String, Integer> statisztika(){
-        HashMap<String, Integer> statisztika = new HashMap<>();
+    public int getVPerc() {
+        return vPerc;
+    }
+    public void setVPerc(int vPerc) {
+        this.vPerc = vPerc;
+    }
 
-        for(String[] kolcsonzes : kolcsonzesek){
-            if(statisztika.containsKey(kolcsonzes[1])){
-                statisztika.put(kolcsonzes[1], statisztika.get(kolcsonzes[1]) + 1);
-            }
-            else{
-                statisztika.put(kolcsonzes[1], 1);
-            }
-        }
 
-        return statisztika;
+    public Kolcsonzes(String nev, String jazon, int eOra, int ePerc, int vOra, int vPerc) {
+        this.nev = nev;
+        this.jazon = jazon;
+        this.eOra = eOra;
+        this.ePerc = ePerc;
+        this.vOra = vOra;
+        this.vPerc = vPerc;
+    }
+
+    public Kolcsonzes(String sor){
+        String[] adatok = sor.split(";");
+
+        setNev(adatok[0]);
+        setJazon(adatok[1]);
+        setEOra(Integer.parseInt(adatok[2]));
+        setEPerc(Integer.parseInt(adatok[3]));
+        setVOra(Integer.parseInt(adatok[4]));
+        setVPerc(Integer.parseInt(adatok[5]));
+
     }
 }
